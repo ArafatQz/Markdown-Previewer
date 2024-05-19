02fb4ea7a5d0b18150f80ad432c57b3bcee5a173
Arafat Gamzawe <86990723+ArafatQz@users.noreply.github.com>
2024-05-19 21:55:19 +0300

    previor.js

--- src/pages/previor.js
+++ src/pages/previor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './pages.css';
-import {marked} from 'marked';
+import { marked } from 'marked';
 
 const Preview = ({ markdown }) => {
   marked.setOptions({
@@ -8,16 +8,16 @@ const Preview = ({ markdown }) => {
   });
 
   const createMarkup = (markdown) => {
-    const html = window.marked.parse(markdown);
+    const html = marked.parse(markdown);
     return { __html: html };
   };
 
-  return <div dangerouslySetInnerHTML={createMarkup(markdown)} id='preview'/>;
+  return <div dangerouslySetInnerHTML={createMarkup(markdown)} id='preview' />;
 };
 
 export default function PreviewPage({ value }) {
   return (
-    <div className='previor' >
+    <div className='previor'>
       <div className='prev-title'><h3>Preview Section</h3></div>
       <Preview markdown={value} />
     </div>
